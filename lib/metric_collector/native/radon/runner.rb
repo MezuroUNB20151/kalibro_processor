d3ed4104e70478cb4b891e38b4c92c74da9d085a
LucasLeite <lucas.srl08@gmail.com>
2015-05-28 15:46:59 -0300

    Fixed conflicts

--- lib/metric_collector/native/radon/runner.rb
+++ lib/metric_collector/native/radon/runner.rb
@@ -2,19 +2,13 @@ module MetricCollector
   module Native
     module Radon
       class Runner
-<<<<<<< HEAD
-        def initialize(attributes={repository_path: nil})
-          @repository_path = attributes[:repository_path]
+        def initialize(attributes={repository_path: nil, chosen_metrics:["raw", "cc", "mi"]})
+          @repository_path = repository_path
+          @chosen_metrics = chosen_metrics
         end
 
         def repository_path
           @repository_path
-=======
-        def initialize(attributes={repository_path: nil, chosen_metrics:["raw", "cc", "mi"]})
-          @repository_path = repository_path
-          @chosen_metrics = chosen_metrics
->>>>>>> 731c780bcab4e9280a562a0485a983c8a00d4490
-        end
 
         def run
           for metric in chosen_metrics do
@@ -23,25 +17,6 @@ module MetricCollector
         end
 
         def clean_output
-<<<<<<< HEAD
-          File.delete('radon_cc_output.json') if File.exists?('radon_cc_output.json')
-          File.delete('radon_mi_output.json') if File.exists?('radon_mi_output.json')
-          File.delete('radon_raw_output.json') if File.exists?('radon_raw_output.json')
-        end
-
-        private
-
-        def run_cyclomatic_complexity
-          Dir.chdir(@repository_path) { `radon cc -s --json . > radon_cc_output.json` }
-        end
-
-        def run_maintainability
-          Dir.chdir(@repository_path) { `radon mi -s --json . > radon_mi_output.json` }
-        end
-
-        def run_raw
-          Dir.chdir(@repository_path) { `radon raw -s --json . > radon_raw_output.json` }
-=======
           for metric in @output_paths do
             File.delete(@output_paths[metric]) if File.exists?(@output_paths[metric])
           end
@@ -61,7 +36,6 @@ module MetricCollector
             end
           end
           @output_paths
->>>>>>> 731c780bcab4e9280a562a0485a983c8a00d4490
         end
 
         private
