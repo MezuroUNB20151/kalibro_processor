d8c7ad05ec7853fe316907271c59c50bf09f8d20
LucasLeite <lucas.srl08@gmail.com>
2015-06-02 12:56:13 -0300

    Implementing parse for cyclomatic complexity

--- lib/metric_collector/native/radon/parser/cyclomatic.rb
+++ lib/metric_collector/native/radon/parser/cyclomatic.rb
@@ -5,24 +5,23 @@ module MetricCollector
         class Cyclomatic < MetricCollector::Native::Radon::Parser::Base
 
           def self.parse(output_path, processing = nil, metric_configuration = nil)
-            #puts "output_path = #{output_path}"
+
             output_file = File.read("#{output_path}/radon_cc_output.json")
             data_hash = JSON.parse(output_file)
 
             data_hash.each do |file_name, result_hash|
               name_prefix = module_name_prefix(file_name)
               result_hash.each do |key|
-                name_suffix = module_name_suffix()
+                name_suffix = module_name_suffix(key['name'])
                 value = key['complexity']
                 module_name = name_prefix + name_suffix
-                module_result = module_result(module_name, Granularity::METHOD, processing)
-                #MetricResult.create(metric: metric_configuration.metric, value: value.to_f, module_result: module_result, metric_configuration_id: metric_configuration.id)
+                granularity = Granularity::METHOD
+                module_result = module_result(module_name, granularity, processing)
+                MetricResult.create(metric: metric_configuration.metric, value: value.to_f, module_result: module_result, metric_configuration_id: metric_configuration.id)
               end
             end
             0
     		  end
-
-    		#MetricResult.create(metric: metric_configuration.metric, value: value.to_f, module_result: module_result, metric_configuration_id: metric_configuration.id)
 
           def self.default_value
             0.0
